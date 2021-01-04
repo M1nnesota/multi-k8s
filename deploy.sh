b2df042e4e71106b4b48c2ceecafa842fe4aff8a
ihber <ihber@smile.fr>
2021-01-04 15:25:24 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t ihorberda/multi-client:latest -t ihorberda/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t ihorberda/multi-server:latest -t ihorberda/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t ihorberda/multi-worker:latest -t ihorberda/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker push ihorberda/multi-client:latest
+docker push ihorberda/multi-server:latest
+docker push ihorberda/multi-worker:latest
+docker push ihorberda/multi-client:$SHA
+docker push ihorberda/multi-server:$SHA
+docker push ihorberda/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=ihorberda/multi-client:$SHA
+kubectl set image deployments/server-deployment server=ihorberda/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=ihorberda/multi-worker:$SHA
